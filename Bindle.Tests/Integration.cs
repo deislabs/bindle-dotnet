8661336e9da7f054eb17f69b4990e7d9bc4892af
Simon Davies <simongdavies@users.noreply.github.com>
2021-07-20 10:51:26 +0100

    Changes after review

--- Bindle.Tests/Integration.cs
+++ Bindle.Tests/Integration.cs
@@ -1,14 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net.Sockets;
-using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
-using Xunit.Abstractions;
-using Xunit.Sdk;
 using static Deislabs.Bindle.GetInvoiceOptions;
 
 namespace Deislabs.Bindle.Tests
@@ -22,134 +14,7 @@ namespace Deislabs.Bindle.Tests
     // Alternatively if you set the variable BINDLE_SERVER_PATH to the relative path of bindle-server then the test will start and stop the server automatically
     // e.g. if bindle is in a peer directory to bindle-dotnet and the debug build is being used then set the variable to "../../../../../bindle/target/debug"
 
-    public class IntegrationFixture : IDisposable
-    {
-        private Process process;
-        private string dataPath;
-        private string origFilePath;
-        private string copyFilePath;
-        public IntegrationFixture()
-        {
-            var bindlePath = Environment.GetEnvironmentVariable("BINDLE_SERVER_PATH");
-            if (!string.IsNullOrEmpty(bindlePath))
-            {
-                var fullPath = Path.GetFullPath(Path.Join(bindlePath, "bindle-server"));
-                dataPath = Path.GetFullPath(Path.Join("../../../data"));
-                Console.WriteLine($"Using Bindle Server Path: {fullPath}");
-                Console.WriteLine($"Using Test Data Path: {dataPath}");
-                var psi = new ProcessStartInfo
-                {
-                    FileName = fullPath,
-                    Arguments = $"-i 127.0.0.1:14044 -d {dataPath}",
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                    RedirectStandardError = true,
-                    RedirectStandardOutput = true
-                };
-                psi.Environment["RUST_LOG"] = "info";
-                process = new Process
-                {
-                    StartInfo = psi
-                };
-                process.OutputDataReceived += (sender, e) =>
-                {
-                    if (!String.IsNullOrEmpty(e.Data))
-                    {
-                        Console.WriteLine(e.Data);
-                    }
-                };
-                process.ErrorDataReceived += (sender, e) =>
-                {
-                    if (!String.IsNullOrEmpty(e.Data))
-                    {
-                        Console.WriteLine(e.Data);
-                    }
-                };
-                process.Start();
-                process.BeginErrorReadLine();
-                process.BeginOutputReadLine();
-                // TODO: something a bit less crappy that this to wait for server to start:
-                Thread.Sleep(5000);
-                Assert.False(process.HasExited);
-
-                // data/invoices/33d7f76b3077de5b72f50d3bf1997d6571770665732ac83b3bede75cf3c5fb67/invoice.toml gets modified by tests so take a copy so it can be reverted at the end.
-
-                origFilePath = Path.Join(dataPath, "invoices/33d7f76b3077de5b72f50d3bf1997d6571770665732ac83b3bede75cf3c5fb67/invoice.toml");
-                copyFilePath = Path.Join(dataPath, "invoices/33d7f76b3077de5b72f50d3bf1997d6571770665732ac83b3bede75cf3c5fb67/invoice.toml.copy");
-                File.Copy(origFilePath, copyFilePath);
-
-            }
-        }
-
-        public void Dispose()
-        {
-
-            // Stop the bindle server
-            try
-            {
-                process?.Kill();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Failed to stop bindle process:{ex}");
-            }
-
-            // Delete the files and dirs that the tests created.
-
-            var dirs = new string[]
-            {
-                Path.Join(dataPath, "invoices/8fb420e8308462667f8e1585b7e768bf2e391aec9a29d13c968849c99ed5b0bf"),
-                Path.Join(dataPath, "parcels/460d5965e4d1909e8c7a3748a414956b7038ab5fd79937c9fcb2b214e6b0160a")
-            };
-
-            foreach (var dir in dirs)
-            {
-                if (Directory.Exists(dir))
-                {
-                    try
-                    {
-                        Directory.Delete(dir, true);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Failed to delete test invoice directory: {dir} : {ex}");
-                    }
-
-                }
-            }
-
-            // Revert the modifications
-            
-            if (File.Exists(copyFilePath))
-            {
-                try
-                {
-                    File.Move(copyFilePath, origFilePath, true);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Failed to delete replace modified test invoice:{ex}");
-                }
-            }
-        }
-    }
-
-    // This is a quick and dirty way of ensuring that CanFetchInvoice runs before CanYankInvoice
-    public class AlphabeticalOrderer : ITestCaseOrderer
-    {
-        public IEnumerable<T> OrderTestCases<T>(IEnumerable<T> testCases) where T : ITestCase
-        {
-            Console.WriteLine($"Test Case Order:");
-            var orderedTestCases = testCases.OrderBy(testCase => testCase.TestMethod.Method.Name);
-            foreach (var tc in orderedTestCases)
-            {
-                Console.WriteLine($"\t{tc.TestMethod.Method.Name}");
-                yield return tc;
-            }
-        }
-    }
-
-    [TestCaseOrderer("Deislabs.Bindle.Tests.AlphabeticalOrderer", "Bindle.Tests")]
+    [TestCaseOrderer("Deislabs.Bindle.Tests.TestPriorityOrderer", "Bindle.Tests")]
     public class Integration : IClassFixture<IntegrationFixture>
     {
         const string DEMO_SERVER_URL = "http://localhost:14044/v1";
@@ -236,7 +101,9 @@ namespace Deislabs.Bindle.Tests
             Assert.Equal(invoice.Groups.Count, fetched.Groups.Count);
         }
 
+        // Make sure CanYankInvoice runs after CanCreateInvoice
         [Fact]
+        [TestPriority(10)]
         public async Task CanYankInvoice()
         {
             var client = new BindleClient(DEMO_SERVER_URL);
